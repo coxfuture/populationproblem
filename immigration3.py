266777372f7d519ce0db63874e1649d22bde5892
isademigod <42841489+isademigod@users.noreply.github.com>
2021-09-24 01:55:48 -0400

    Update immigration3.py

--- immigration3.py
+++ immigration3.py
@@ -1,33 +1,86 @@
 import random
 import matplotlib.pyplot as plt
+import numpy as np
 import math
+import scipy
+from scipy import optimize
+from scipy import stats
 
-border = []
-num_left = []
-immigrants = 1000
-for i in range(17):
-    print("____________________________")
-    print("In iteration",i,"there are",immigrants,"immigrants left")
-    num_leave = math.trunc(immigrants * random.random())    
-    print(num_leave,"stay at the border")
-    immigrants = immigrants - num_leave
-    print(immigrants,"keep going")
-    border.append(i)
-    num_left.append(immigrants)
-    print("____________________________")
 
-print("x-axis:",border)
-print("y-axis:",num_left)
+data = {}
+num_trials = 20
+num_borders = 200 
+loopct = ((num_trials*num_borders)/100)
+for j in range(num_trials):
+    #initialize data
+    num_left = [1000]
+    border = [0]
+    immigrants = 1000
+    for i in range(num_borders):
+        print("test",j,"loop",i,"{:.2f}".format((j*num_borders+i)/(loopct)),"%\ done")
+        #avoid division by zero
+        if immigrants == 0:
+            num_left.append(0)
+            border.append(i+1)
+        else:    
+            #get a random number of immigrants to leave behind, based on a normal distribution
+            normal_rand = (sum(np.random.binomial(1, 0.5, math.trunc(immigrants))==0)/immigrants)
+            num_leave = math.trunc(immigrants * normal_rand)   
+            
+            #r = (births-deaths)/population
+            #imms = imms*e^(rt)
+            r = (0.0002*i)
+            immigrants = (immigrants - num_leave) + math.e**(r*i)
+            #data collection
+            border.append(i+1)
+            num_left.append(immigrants)
+    data["Trial "+str(j+1)] = num_left
+    
 
-plt.plot(x, y)
- 
-# naming the x axis
-plt.xlabel('x - axis')
-# naming the y axis
-plt.ylabel('y - axis')
- 
-# giving a title to my graph
-plt.title('My first graph!')
- 
-# function to show the plot
+plt.xlabel('Border #')
+plt.ylabel('Number of immigrants left') 
+plt.title('Number left at each border')
+
+colors = list("rgbcmyrgbcmy")
+for imms_left in data.values():
+    #print(imms_left)
+    #print(border)
+    x = border
+    y = imms_left
+    plt.scatter(x,y,color="grey")
+    plt.plot(x,y,color="grey")
+
+#exponential regression
+A_vals = []
+B_vals = []
+C_vals = []
+#iterate over y-values from repeated trials to get an average for a and b where y=a*e^bx
+for y in data.values():
+    x_data = np.array(border)
+    y_data = np.array(y)
+    popt, pcov = scipy.optimize.curve_fit(lambda t,a,b,c: a*np.exp(b*t)+np.exp(c*t),  x_data,  y_data,  p0=(1000, -0.5, 0.02))
+    print(popt)
+    a = popt[0]
+    b = popt[1]
+    c = popt[2]
+    A_vals.append(a)
+    B_vals.append(b)
+    C_vals.append(c)
+
+#get average a and b values
+eqA = sum(A_vals)/len(A_vals)
+eqB = sum(B_vals)/len(B_vals)
+eqC = sum(C_vals)/len(C_vals)
+
+print("the equation is y=",eqA,"*e^(",eqB,"*x)+e^(",eqC,"x)")
+#plot line of best fit
+x_1 = np.array(border)
+y_1 = eqA * math.e **(eqB * x_1)+ math.e **(eqC * x_1)
+plt.plot(x_1, y_1, color="red")
+# x_2 = np.array(border)
+# y_2 = 20 + 980 * math.e **(-0.5 * x_2)
+# plt.plot(x_2, y_2, color="blue",label="expected")
+
+
+plt.legend(data.keys())
 plt.show()
